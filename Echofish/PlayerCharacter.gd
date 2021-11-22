a7a3271c92545520fe7807a0f1464ec4e853711f
fishies <xieandrew@protonmail.com>
2021-11-22 15:46:15 -0800

    Add PlayerCharacter with functionality for following cursor

---
+++ Echofish/PlayerCharacter.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var speed = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = self.position.direction_to(get_viewport().get_mouse_position())
+	translate(direction * speed)
