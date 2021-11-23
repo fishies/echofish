f6760d72dc59ffbf30e1c935f6a90829fbfb744c
fishies <xieandrew@protonmail.com>
2021-11-22 17:29:05 -0800

    Implement basic hitbox detection and timer-based health

--- Echofish/PlayerCharacter.gd
+++ Echofish/PlayerCharacter.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Declare member variables here.
 var speed = 100.0
+var health = 30.0 # time remaining to live
+var survived = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +11,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# tick down health by delta
+	survived += delta
+	health -= delta
+	# check if player is still alive
+	if health <= 0.0:
+		# pop up a UI for play again? with your survived time
+		survived = 0.0
+		return
+	# move towards mouse cursor
 	var direction = self.position.direction_to(get_viewport().get_mouse_position())
 	translate(direction * speed * delta)
 	if direction.x * scale.x < 0:
@@ -18,5 +27,13 @@ func _process(delta):
 
 func _on_Predator_area_entered(area):
 	#implement death here
-	print("u ded")
-	pass
+	if area == self:
+		print("u ded")
+		health = 0.0
+	# death animations? particools?
+
+func _on_Food_area_entered(area):
+	#food eating behavior
+	if area == self:
+		print("u eat")
+		health += 2.0
