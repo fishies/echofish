c93eb9928c4c5cd4a69d9153e4e97908d5e04659
fishies <xieandrew@protonmail.com>
2021-11-26 17:45:01 -0800

    Reorganize game.gd

--- Echofish/game.gd
+++ Echofish/game.gd
@@ -11,15 +11,24 @@ var random = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	random.randomize()
+	#get_tree().paused = true
+
+func startGame():
+	while get_tree().get_nodes_in_group("FoodList").size() < maxSpawns:
+		spawnFood()
+	get_tree().paused = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func spawnFood():
+	var newFood = foodScene.instance()
+	add_child(newFood)
+	newFood.add_to_group("FoodList")
+	newFood.set_position(Vector2(random.randi() % maxXBound, random.randi() % maxYBound))
+
 func _on_SpawnTimer_timeout():
 	if random.randf() < spawnChance:
 		if get_tree().get_nodes_in_group("FoodList").size() < maxSpawns:
-			var newFood = foodScene.instance()
-			add_child(newFood)
-			newFood.add_to_group("FoodList")
-			newFood.set_position(Vector2(random.randi() % maxXBound, random.randi() % maxYBound))
+			spawnFood()
