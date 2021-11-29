8cdeb9ca9deb10fab09153f5c94a74adcc57e0d3
fishies <xieandrew@protonmail.com>
2021-11-28 19:44:28 -0800

    Fish now glow briefly after getting pinged by sonar

--- Echofish/Food.gd
+++ Echofish/Food.gd
@@ -1,16 +1,15 @@
 extends Area2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var glowDecayRate = 8.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if get_node("Light2D").energy > 0.0:
+		get_node("Light2D").energy -= delta * glowDecayRate
 
 func _on_Food_area_entered(area):
 	if area.name == "PlayerCharacter":
@@ -19,3 +18,5 @@ func _on_Food_area_entered(area):
 			area.get_node("EatSound").play()
 			area.get_node("Vitals").health += 2.0
 			queue_free()
+	if area.name == "SonarHitbox":
+		get_node("Light2D").energy = 16.0
--- Echofish/Predator.gd
+++ Echofish/Predator.gd
@@ -1,15 +1,20 @@
 extends Area2D
 
+var glowDecayRate = 8.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if get_node("Light2D").energy > 0.0:
+		get_node("Light2D").energy -= delta * glowDecayRate
 
 func _on_Predator_area_entered(area):
 	if area.name == "PlayerCharacter":
 		#print("u ded")
 		area.get_node("Vitals").health = 0.0
-	# death animations? particools?
+		# death animations? particools?
+	if area.name == "SonarHitbox":
+		get_node("Light2D").energy = 16.0
